b9241dd56c3660aeb016f0dacf390239dc20529c
Miguel Pagola <mipagola@gmail.com>
2019-06-11 19:54:01 +0200

    CORS allowed

--- src/main/kotlin/poc/yaas/anagrams/AnagramSolverController.kt
+++ src/main/kotlin/poc/yaas/anagrams/AnagramSolverController.kt
@@ -1,10 +1,12 @@
 package poc.yaas.anagrams
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin
 class AnagramSolverController(
         private val anagramSolverService: AnagramSolverService
 ) {
